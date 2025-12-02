53d3b00269a3f7ee357a0703fbfc74847be195cb
sunfly04 <sunglow_fy@outlook.com>
2025-12-02 20:44:01 +0800

    update media and page

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,23 @@
-window.HELP_IMPROVE_VIDEOJS = false;
+// window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+// var INTERP_BASE = "./static/interpolation/stacked";
+// var NUM_INTERP_FRAMES = 240;
 
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
+// var interp_images = [];
+// function preloadInterpolationImages() {
+//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+//     interp_images[i] = new Image();
+//     interp_images[i].src = path;
+//   }
+// }
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
+// function setInterpolationImage(i) {
+//   var image = interp_images[i];
+//   image.ondragstart = function() { return false; };
+//   image.oncontextmenu = function() { return false; };
+//   $('#interpolation-image-wrapper').empty().append(image);
+// }
 
 
 $(document).ready(function() {
@@ -59,7 +59,7 @@ $(document).ready(function() {
     for(var i = 0; i < carousels.length; i++) {
     	// Add listener to  event
     	carousels[i].on('before:show', state => {
-    		console.log(state);
+    		// console.log(state);
     	});
     }
 
@@ -68,7 +68,7 @@ $(document).ready(function() {
     if (element && element.bulmaCarousel) {
     	// bulmaCarousel instance is available as element.bulmaCarousel
     	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
+    		// console.log(state);
     	});
     }
 
@@ -79,14 +79,33 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 
+    // Lazy load/play videos
+    var videos = document.querySelectorAll('video');
+    var observer = new IntersectionObserver(function(entries) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          if (entry.target.paused) {
+            entry.target.play().catch(function(e) { console.log("Play error:", e); });
+          }
+        } else {
+          if (!entry.target.paused) {
+            entry.target.pause();
+          }
+        }
+      });
+    }, { threshold: 0.1 });
+
+    videos.forEach(function(video) {
+      observer.observe(video);
+    });
 })
